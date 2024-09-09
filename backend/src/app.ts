e3a63c2346fc94056d8881ca383e9e8374867e69
Thapelo-git <chabathapelo1@gmail.com>
2024-09-09 14:29:55 +0200

    commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,30 @@
 import "dotenv/config";
 import express, { NextFunction,Request,Response } from "express";
 import notesRouters from './routes/notes'
+import userRouters from './routes/Users';
 import morgan from "morgan"
 import createHttpError,{isHttpError} from "http-errors";
-
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use(session({
+  secret:env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized:false,
+  cookie:{
+    maxAge:60*60*1000,
+  },
+  rolling:true,
+  store:MongoStore.create({
+    mongoUrl:env.MONGO_CONNECTION_STRING
+  })
+}))
+app.use("/api/users",userRouters)
 app.use('/api/notes',notesRouters)
 
 app.use((req,res,next)=>{
